234e6078acda776955367da966459cd227fc746c
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-12-03 20:08:30 +0100

    added login logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import toast from "react-hot-toast";
 
-const page = () => {
+export default function Login() {
+  const router = useRouter();
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const [buttondisabled, setButtonDisabled] = React.useState(false);
+  const [Loading, setLoading] = React.useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("login success", response.data);
+      toast.success("Login Success");
+      router.push("/profile ");
+    } catch (error: any) {
+      console.log("login failled", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
   return (
-    <div>Login page</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-white">
+      <h1 className="mb-4">Login</h1>
+      <hr />
+      <label htmlFor="userName">email</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600 "
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="userName">password</label>
+      <input
+        className="p-2 text-black border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600 "
+        type="text"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus-border-gray-600 "
+      >
+        Login
+      </button>
+      <Link href="/signup">Visit Signup page</Link>
+    </div>
+  );
 }
-
-export default page
